7e8d0dc968ab4f3197f87d7b87cd464c53ca87ce
LETICYA SILVA DIAS <leticya_dias@estudante.sesisenai.org.br>
2025-05-14 22:20:29 -0300

    exercicio sobre APIS

--- exercicio02/scripts.js
+++ exercicio02/scripts.js
@@ -2,12 +2,22 @@ fetch("https://jsonplaceholder.typicode.com/users")
 .then(response => response.json())
 .then(usuario =>{
     const container = document.querySelector("#container")
-    const user = primeiros10.map(item => {
+    const usuarios = usuario.slice(0)
+    const user = usuarios.map(item => {
         return `
-         <div class="card">
+         <div class="users">
            <img src="https://placehold.co/600x400" alt=${item.title}/>
-           <h2>${item.title}</h2>
+           <h2>${item.name}</h2>
+           <p><strong> Username:</strong> ${item.username}
+           <p><strong> Email:</strong> ${item.email}
+           <p><strong> Telefone:</strong> ${item.phone}
+           <p><strong> Website:</strong> ${item.website}
+           <p><strong> Empresa:</strong> ${item.company.name}
+           <p><strong> Endereço:</strong> ${item.address.street}, ${item.address.suite}, ${item.address.city}
+
          </div>
         `
     })
+
+    container.innerHTML = user.join("")
 })
